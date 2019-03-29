c2aaa4cfb3f52591ca3c064b65ffd3255e37a06a
=Gulnar <gulnar.hesenzade@gmail.com>
2019-03-29 14:49:05 +0300

    Money Exchange

--- src/index.js
+++ src/index.js
@@ -2,4 +2,24 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+	
+	if(currency<=10000)
+	{
+		let array={'H':50,'Q':25,'D':10,'N':5,'P':1};
+
+        let result ={};
+
+		for (let key in array)
+		{
+			let num = Math.floor(currency/array[key]);
+			if(num>0)
+			{
+				result[key] = num;
+				currency= currency - num*array[key];
+			}
+		}
+		return result;
+	}
+	else
+		return {error: "You are rich, my friend! We don't have so much coins for exchange"}
 }
